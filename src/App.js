8a83da2fab85092ebde28893b21db0bb6de436ab
Rennlad <rennickman@gmail.com>
2022-06-02 15:58:39 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Profile from './pages/profile/Profile';
+
 
 
 
 function App() {
     return (
         <div>
-            <Home />
+            <Router>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='login' element={<Login />} />
+                    <Route path='register' element={<Register />} />
+                    <Route path='profile' element={<Profile/>} />
+                </Routes>
+            </Router>
         </div>
     );
 }
